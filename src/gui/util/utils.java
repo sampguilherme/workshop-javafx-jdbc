eb7a5108c5026a25f532fe8826d9d56a9680012b
Guilherme Sampaio <guilherme.sampaio324@hotmail.com>
2026-07-12 18:19:42 -0300

    Saving a new Department

--- src/gui/util/utils.java
+++ src/gui/util/utils.java
@@ -8,4 +8,13 @@ public class utils {
 	public static Stage currentStage(ActionEvent event) {
 		return (Stage) ((Node) event.getSource()).getScene().getWindow();
 	}
+	
+	public static Integer tryParseToInt(String str) {
+		try {
+			return Integer.parseInt(str);
+		}
+		catch (NumberFormatException e) {
+			return null;
+		}
+	}
 }
